045f6419c8f6e8b97ccae7a8cad4200391e20c32
Taras Tymoshchuk <taras.tymoshchuk@eliftech.com>
2017-08-15 18:17:43 +0300

    Update configs

--- src/components/configs/configs.component.js
+++ src/components/configs/configs.component.js
@@ -11,13 +11,38 @@ const ConfigsComponent = {
 export default ConfigsComponent;
 
 /* @ngInject */
-function ConfigsCtrl($scope, $location, appConfig) {
+function ConfigsCtrl($location, $http, $log) {
   const vm = this;
+  vm.loading = false;
+  const mobileNumberPrefix = '+380';
+  const mobileNumberMask = `${mobileNumberPrefix} 99 999 99 99`;
 
-  vm.mobileNumberMask = $location.search().mobileNumberMask || appConfig.mobileNumberMask;
-  vm.mobileNumberPrefix = $location.search().mobileNumberPrefix || appConfig.mobileNumberPrefix;
+  vm.mobileNumberMask = $location.search().mobileNumberMask || mobileNumberMask;
+  vm.mobileNumberPrefix = $location.search().mobileNumberPrefix || mobileNumberPrefix;
 
-  // $scope.$watch('vm.mobileNumber', (value) => {
-  //   vm.config.mobileNumber = value && `${vm.mobileNumberPrefix}${value}`;
-  // });
+  vm.$onInit = () => {
+    getConfigs();
+  };
+
+  function getConfigs() {
+    $http.get('https://pvtxez7kl7.execute-api.eu-west-1.amazonaws.com/prod/getSmartCoolerConfig').then((configs) => {
+      vm.config = configs;
+      vm.config.TWILLIO_PHONE_NUMBER_TO =
+              vm.config.data.TWILLIO_PHONE_NUMBER_TO.toString()
+                  .substring(4, vm.config.data.TWILLIO_PHONE_NUMBER_TO.length);
+    }).catch((error) => {
+      vm.error = error;
+      $log.log('ERROR!', error);
+    });
+  }
+
+  vm.updateConfigs = (config) => {
+    $http.put('https://23kl9657m1.execute-api.eu-west-1.amazonaws.com/prod/setSmartCoolerConfig', config)
+        .then(() => {
+          getConfigs();
+        }).catch((error) => {
+          vm.error = error;
+          $log.log('ERROR!', error);
+        });
+  };
 }
